b82d9af6d537f89f740d90de53509aa523079fd0
Arthur Gonthier <arthur@qeels.io>
2025-05-09 00:01:41 +0200

    Third commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/landing-cesarvivaldi/', // Ajoute ici le chemin correspondant à ton dépôt GitHub
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
